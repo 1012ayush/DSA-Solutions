d17af153ac2bafe24c1ccbb5c47eae65cac1b746
1012ayush <sumitkatiyar783@gmail.com>
2026-06-28 16:41:47 +0530

    Time: 5 ms (79.58%), Space: 46.1 MB (33.78%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
          if (x < 0) return false;
-        String str = String.valueOf(x);
+ /*       String str = String.valueOf(x);
         int n = str.length();
        for(int i = 0 ; i < n/2 ; i++){
         if(str.charAt(i) != str.charAt(n-1-i)){
@@ -9,6 +9,14 @@ class Solution {
         }
        
        } 
-       return true;
+       return true; */
+
+       int original = x;
+       long reversed = 0;
+       while(x != 0){
+      reversed =   (reversed * 10) + (x %10) ;
+        x = x/10;
+       }
+       return original == reversed ;
     }
 }

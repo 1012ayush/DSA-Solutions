de2524ba7a6bfc19a20eba099f217b23f051b817
1012ayush <sumitkatiyar783@gmail.com>
2026-06-30 18:33:51 +0530

    Time: 5 ms (8.65%), Space: 47.2 MB (97.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList ;
 class Solution {
     public void moveZeroes(int[] nums) {
-      int j = 0;
+    /*  int j = 0;
       for(int i = 0 ; i< nums.length ; i++){
         if(nums[i] != 0){
             int temp = nums[i];
@@ -8,6 +9,19 @@ class Solution {
             nums[j] = temp;
             j++ ;
         }
-      }
+      } */
+ArrayList<Integer> temp = new ArrayList<>();
+for( int i = 0 ; i < nums.length ; i++){
+    if(nums[i] != 0){
+        temp.add(nums[i]);
+    }
+}
+for(int i = 0 ; i < temp.size() ; i++){
+    nums[i] = temp.get(i);
+}
+int tn = temp.size();
+for(int i = tn ; i < nums.length ; i++){
+    nums[i] = 0 ;
+}
         }
     }

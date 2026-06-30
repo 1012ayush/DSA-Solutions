673622f007caa8b004e9b7b466d56376ebb9af1d
1012ayush <sumitkatiyar783@gmail.com>
2026-06-30 12:34:09 +0530

    Enhance comments for array sorted and rotated logic
    Added detailed comments to explain the logic for checking if an array is sorted and rotated.

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -12,3 +12,33 @@ class Solution {
         return true ;
     }
 }
+
+/* i = 0: Check nums[0] > nums[(0 + 1) % 5]
+
+Index math: 1 % 5 = 1
+
+Check values: nums[0] > nums[1] ➔ 4 > 5 ➔ No drop
+
+i = 1: Check nums[1] > nums[(1 + 1) % 5]
+
+Index math: 2 % 5 = 2
+
+Check values: nums[1] > nums[2] ➔ 5 > 1 ➔ Drop 1!
+
+i = 2: Check nums[2] > nums[(2 + 1) % 5]
+
+Index math: 3 % 5 = 3
+
+Check values: nums[2] > nums[3] ➔ 1 > 2 ➔ No drop
+
+i = 3: Check nums[3] > nums[(3 + 1) % 5]
+
+Index math: 4 % 5 = 4
+
+Check values: nums[3] > nums[4] ➔ 2 > 6 ➔ No drop
+
+i = 4 (The Wrap-Around): Check nums[4] > nums[(4 + 1) % 5]
+
+Index math: 5 % 5 = 0 (This loops back to the start!)
+
+Check values: nums[4] > nums[0] ➔ 6 > 4 ➔ Drop 2!    If Drop > 1 then it will return false as it is not properly sorted .*/

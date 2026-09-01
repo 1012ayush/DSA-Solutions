9cee22950f4e39dcf55a6874c2b3e1afacf66a1e
1012ayush <sumitkatiyar783@gmail.com>
2026-09-02 00:09:58 +0530

    Time: 6 ms (80.79%), Space: 44.5 MB (37.21%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String reverseWords(String s) {/* // \\s+ we used is the regex as s is used for one whitespace but \\s+ means many whitespaces a regex thing . 
+    public String reverseWords(String s) { // \\s+ we used is the regex as s is used for one whitespace but \\s+ means many whitespaces a regex thing . 
         String[]  words = s.trim().split("\\s+"); //s.trim trims the edges spaces and in between if we find the spaces it deals with the split and the the , sky , is , blue is now separate words and letter we reversing and appending by using the StringBuilder.
         StringBuilder sb = new StringBuilder();
         for(int i = words.length - 1; i >= 0 ; i--){
@@ -8,9 +8,9 @@ class Solution {
             sb.append(" "); // here it inserts spaces after the every word but it don't want to insert space after the last word which is our first word .
            }
         }
-        return sb.toString() ; */
+        return sb.toString() ; 
 
-        int i = s.length() - 1;
+     /*   int i = s.length() - 1;
         StringBuilder sb = new StringBuilder();
         while(i >= 0){
             while(i >= 0 && s.charAt(i)==' '){
@@ -30,6 +30,6 @@ class Solution {
             }
             sb.append(s.substring(left , right + 1));
         }
-        return sb.toString();
+        return sb.toString(); */
     }
 }
